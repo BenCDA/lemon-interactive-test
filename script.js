07ab8c0b85f77909b5be42406c2d041a2bbedb80
BenCDA <cardosobenjamin01@gmail.com>
2024-03-20 21:29:28 +0100

    (fix): fix bouton menu burger

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
     const burgerMenu = document.querySelector('.burger-menu');
     const navbarMenu = document.querySelector('.navbar-menu');
+    const searchBar = document.querySelector('.burger-menu-search-bar');
 
     burgerMenu.addEventListener('click', function () {
         navbarMenu.classList.toggle('active');
+        searchBar.classList.toggle('active');
+        
+        if (navbarMenu.classList.contains('active')) {
+            burgerMenu.classList.add('close');
+        } else {
+            burgerMenu.classList.remove('close');
+        }
+    });
+    
+    document.addEventListener('click', function(event) {
+        if (!navbarMenu.contains(event.target) && !burgerMenu.contains(event.target) && !searchBar.contains(event.target)) {
+            navbarMenu.classList.remove('active');
+            searchBar.classList.remove('active');
+            burgerMenu.classList.remove('close');
+        }
     });
 });
